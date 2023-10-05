57e3bb5d8a447774c31c398bf5ee6a1b9a840ed6
Allison Lamb <allison.lamb624@gmail.com>
2023-10-05 00:11:22 -0400

    Defining Data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import ParkingLotForm from "./Components/ParkingLotForm/ParkingLotForm";
 import ParkingLotList from "./Components/ParkingLotList/ParkingLotList";
+
+
 import './App.css';
 
 function App() {
+  let [parkingLotItems, setParkingLotItems] = useState([
+    {
+        date: "2023-10-04",
+        priority: "Low",
+        link: "https://google,com/",
+        description: "Knower of All",
+    },
+    {
+        date: "2023-12-25",
+        priority: "High",
+        link: "https://netflix.com/",
+        description: "The Christmas Movie",
+    },
+  ]);
+  
   return (
     <div className="App">
       <header>
