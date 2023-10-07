32e59deff12974be9b118629e32f9045c79213f5
Allison Lamb <allison.lamb624@gmail.com>
2023-10-07 19:09:33 -0400

    CSS

--- src/Components/ParkingLotList/ParkingLotItem.jsx
+++ src/Components/ParkingLotList/ParkingLotItem.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import ParkingLotItem from './ParkingLotItem';
 
-export default function ParkingLotList({ parkingLotItems }) {
-
-    let ParkingLotItemsJsx = parkingLotItems.map( item => <ParkingLotItem />);
-
-    return (
-<div>
-    ParkingLotList
-    { ParkingLotItemsJsx }
-</div>
+export default function ParkingLotItem({ id, date, priority, link, description }) {
+return (
+    <section className="parking-lot-item-container">
+        <p className="parking-lot-item-field">Date: { date }</p>
+        <p className="parking-lot-item-field">Priority: { priority }</p>
+        <p className="parking-lot-item-field">Description: { description }</p>
+        <p className="parking-lot-item-field">
+            Link:&nbsp;
+            <a href={link} target="_blank" className="parking-lot-item-anchor">
+                { link }
+            </a>
+        </p>
+    </section>
 );
-} 
+}
--- src/Components/ParkingLotList/ParkingLotList.jsx
+++ src/Components/ParkingLotList/ParkingLotList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ParkingLotItem from './ParkingLotItem';
+
+import './ParkingLot.css';
 
 export default function ParkingLotList({ parkingLotItems }){
+    let ParkingLotItemsJsx = parkingLotItems.map( item =>
+        <ParkingLotItem key={item.id} {...item} />
+        );
+
     return (
-        <div>ParkingLotList</div>
+        <section classname="parking-lot-list-container">
+            { ParkingLotItemsJsx }
+        </section>
     );
 }
