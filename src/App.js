af599d5a71987920ccc740e07d473d7f63e4e5d3
Allison Lamb <allison.lamb624@gmail.com>
2023-10-07 18:45:25 -0400

    fixing & edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import ParkingLotForm from "./Components/ParkingLotForm/ParkingLotForm";
 import ParkingLotList from "./Components/ParkingLotList/ParkingLotList";
 
@@ -8,50 +9,20 @@ import './App.css';
 function App() {
   let [ParkingLotItems, setParkingLotItems] = useState([
     {
+        id: nanoid(),
         date: "2023-10-04",
         priority: "Low",
         link: "https://google,com/",
         description: "Knower of All",
     },
     {
+        id: nanoid(),
         date: "2023-12-25",
         priority: "High",
         link: "https://netflix.com/",
         description: "The Christmas Movie",
     },
-  ]);
-
-  let parkingLotItems = [
-    {
-      date: "2023-10-04",
-      priority: "Low",
-      link: "https://google,com/",
-      description: "Knower of All",
-  },
-  {
-      date: "2023-12-25",
-      priority: "High",
-      link: "https://netflix.com/",
-      description: "The Christmas Movie",
-  },
-]
-
-  let listItems = parkingLotItems.map(
-
-    (item) => `
-    <li>
-       <a href="${item.link}">
-          ${item.description} (${item.date} - ${item.priority})
-       </a>    
-    </li>`
-  ).join('');
-    
-  console.log(listItems);
-
-  let html = `<ul>${listItems}</ul>`;
-
-  console.log(html); 
-   
+  ]);  
   return (
     <div className="App">
       <header>
